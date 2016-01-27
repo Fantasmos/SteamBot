15c95cdd5a7bbcffb26231e5ccf2bcf3b8b34dca
Topher Winward <hi@topher.io>
2016-01-27 16:45:20 +0000

    Testing "DoesMessageStartWith"

---
+++ SteamBot/VBot/Utilities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SteamBot.VBot
+{
+    public class Utilities
+    {
+        public bool DoesMessageStartWith(string Message, string[] Comparison)
+        {
+            foreach (string CommandWord in Comparison)
+            {
+                if (Message.StartsWith(CommandWord, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ VBotTests/VBot/UtilitiesTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteamBot.VBot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamBot.VBot.Tests
+{
+    [TestClass()]
+    public class UtilitiesTests
+    {
+        [TestMethod()]
+        public void DoesMessageStartWithTest()
+        {
+            Assert.IsFalse(new Utilities().DoesMessageStartWith("this is my message", new string[] { }));
+            Assert.IsFalse(new Utilities().DoesMessageStartWith("this is my message", new string[] { "a", "list", "of", "words" }));
+            Assert.IsTrue(new Utilities().DoesMessageStartWith("this is my message", new string[] { "this" }));
+            Assert.IsTrue(new Utilities().DoesMessageStartWith("this is my message", new string[] { "some", "words", "this" }));
+        }
+    }
+}
