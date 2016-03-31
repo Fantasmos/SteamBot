8bb9c9d6284bb809259524b549cf281c26dd71cd
Fantasmos <andromedam51413>
2016-03-31 19:53:19 +1100

    Fix Upload Method

--- SteamBot/SheetHandler.cs
+++ SteamBot/SheetHandler.cs
@@ -16,9 +16,9 @@ namespace SteamBot
         /// New Method To get Worksheet
         /// </summary>
         /// <returns></returns>
-        public WorksheetEntry GetWorksheet(OAuth2Parameters parameters, string IntegrationName, string SpreadSheetURI, SpreadsheetsService service)
+        public  WorksheetEntry GetWorksheet(OAuth2Parameters parameters, string IntegrationName, string SpreadSheetURI, SpreadsheetsService service)
         {
-            
+
             SpreadsheetQuery query = new SpreadsheetQuery(SpreadSheetURI);
             SpreadsheetFeed feed = service.Query(query);
             SpreadsheetEntry spreadsheet = (SpreadsheetEntry)feed.Entries[0];
@@ -27,64 +27,57 @@ namespace SteamBot
             return worksheet;
         }
 
+
         //public void UploadSheet(bool Forcesync, Dictionary<string, Tuple<string, SteamID, string, bool>> Maplist, String IntegrationName, string CLIENT_ID,string CLIENT_SECRET, string REDIRECT_URI, string SCOPE, string GoogleAPI)
-        public void UploadSheet(bool Forcesync, Dictionary<string, Tuple<string, string, string, bool>> Maplist, OAuth2Parameters parameters, string IntegrationName, string SpreadSheetURI)
+        public  void UploadSheet(bool Forcesync, Dictionary<string, Tuple<string, string, string, bool>> Maplist, OAuth2Parameters parameters, string IntegrationName, string SpreadSheetURI)
         {
             GOAuth2RequestFactory requestFactory = new GOAuth2RequestFactory(null, IntegrationName, parameters);
             SpreadsheetsService service = new SpreadsheetsService(IntegrationName);
-            
+
             string accessToken = parameters.AccessToken;
             service.RequestFactory = requestFactory;
 
             WorksheetEntry worksheet = GetWorksheet(parameters, IntegrationName, SpreadSheetURI, service);
 
-           
-            worksheet.Cols = 5;
-            worksheet.Rows = Convert.ToUInt32(Maplist.Count + 1);
+            worksheet.Rows = Convert.ToUInt32(Maplist.Count + 2);
             worksheet.Update();
 
-            CellQuery cellQuery = new CellQuery(worksheet.CellFeedLink);
-            cellQuery.ReturnEmpty = ReturnEmptyCells.yes;
-            CellFeed cellFeed = service.Query(cellQuery);
-            CellFeed batchRequest = new CellFeed(cellQuery.Uri, service);
+            AtomLink listFeedLink = worksheet.Links.FindService(GDataSpreadsheetsNameTable.ListRel, null);
 
-            int Entries = 1;
+            ListQuery listQuery = new ListQuery(listFeedLink.HRef.ToString());
+
+            ListFeed listFeed = service.Query(listQuery);
+
+            int Increments = 1;
+
+            if (listFeed.Entries.Count.ToString() == "1")
+            {
+                Increments = 0;
+            }
+
+            ListEntry NewRow = new ListEntry();
 
             foreach (var item in Maplist)
             {
-                Entries = Entries + 1;
-                foreach (CellEntry cell in cellFeed.Entries)
-                {
-                    if (cell.Title.Text == "A" + Entries.ToString())
-                    {
-                        cell.InputValue = item.Key;
-                    }
-                    if (cell.Title.Text == "B" + Entries.ToString())
-                    {
-                        cell.InputValue = item.Value.Item1;
 
-                    }
-                    if (cell.Title.Text == "C" + Entries.ToString())
-                    {
-                        cell.InputValue = item.Value.Item2.ToString();
+                ListEntry row = (ListEntry)listFeed.Entries[Increments];
 
-                    }
-                    if (cell.Title.Text == "D" + Entries.ToString())
-                    {
-                        cell.InputValue = item.Value.Item3.ToString();
+                row.Elements[1].Value = item.Key;
+                row.Elements[2].Value = item.Value.Item1;
+                row.Elements[3].Value = item.Value.Item2;
+                row.Elements[4].Value = item.Value.Item3;
+                row.Elements[5].Value = item.Value.Item4.ToString();
 
-                    }
-                    if (cell.Title.Text == "E" + Entries.ToString())
-                    {
-                        cell.InputValue = item.Value.Item4.ToString();
-                    }
-                }
+                row.Update();
+                //service.Insert(listFeed, row);
+
+                Increments = Increments + 1;
             }
 
-            cellFeed.Publish();
-            CellFeed batchResponse = (CellFeed)service.Batch(batchRequest, new Uri(cellFeed.Batch));
+            listFeed.Publish();
+
         }
-        public Dictionary<string, Tuple<string, string, string, bool>> SyncSheetDownload(string IntegrationName, OAuth2Parameters paramaters, string SpreadSheetURI)
+        public  Dictionary<string, Tuple<string, string, string, bool>> SyncSheetDownload(string IntegrationName, OAuth2Parameters paramaters, string SpreadSheetURI)
         {
             GOAuth2RequestFactory requestFactory = new GOAuth2RequestFactory(null, IntegrationName, paramaters);
             SpreadsheetsService service = new SpreadsheetsService(IntegrationName);
@@ -95,7 +88,7 @@ namespace SteamBot
             WorksheetEntry worksheet = GetWorksheet(paramaters, IntegrationName, SpreadSheetURI, service);
 
             Dictionary<string, Tuple<string, string, string, bool>> OnlineMapList = new Dictionary<string, Tuple<string, string, string, bool>>();
-            
+
             string map = "MapNameError";
             string URL = "URL ERROR";
             string UserSteamID = "0";
@@ -107,30 +100,27 @@ namespace SteamBot
             ListQuery listQuery = new ListQuery(listFeedLink.HRef.ToString());
             ListFeed listFeed = service.Query(listQuery);
 
-            //TODO GENERAL CLEANUP
-
             foreach (ListEntry row in listFeed.Entries)
             {
 
-                
-                    map = row.Elements[0].Value;
-                    URL = row.Elements[1].Value;
-                    UserSteamID = row.Elements[2].Value;
-                    Note = row.Elements[3].Value;
+                map = row.Elements[1].Value;
+                URL = row.Elements[2].Value;
+                UserSteamID = row.Elements[3].Value;
+                Note = row.Elements[4].Value;
 
-                    if (row.Elements[3].Value == "TRUE")
-                    {
-                        MapUploadStatus = true;
-                    }
-                    else
-                    {
-                        MapUploadStatus = false;
-                    }
-                    if (!OnlineMapList.ContainsKey(map))
-                    {
-                        OnlineMapList.Add(map, new Tuple<string, string, string, bool>(URL, UserSteamID, Note, MapUploadStatus));
-                    }
-                
+                if (row.Elements[5].Value == "TRUE")
+                {
+                    MapUploadStatus = true;
+                }
+                else
+                {
+                    MapUploadStatus = false;
+                }
+                if (!OnlineMapList.ContainsKey(map))
+                {
+                    OnlineMapList.Add(map, new Tuple<string, string, string, bool>(URL, UserSteamID, Note, MapUploadStatus));
+                }
+
             }
             return OnlineMapList;
 
